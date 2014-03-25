0506730964b8333046dfae4c7af7ad507a5203c6
Saul <s.shanabrook@gmail.com>
2014-03-24 22:10:09 -0400

    add lein-midje requirement so it wont fail on travis

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :min-lein-version "2.0.0"
   :eastwood {:exclude-namespaces [:test-paths]}
   :profiles {:test {:plugins [[jonase/eastwood "0.1.0"]
-                              [lein-cloverage "1.0.2"]]
+                              [lein-cloverage "1.0.2"]
+                              [lein-midje "3.1.1"]]
                     :dependencies [[midje "1.5.0"]]}})
