131925309cbb5a0445ea1dc836b55b9dc3758648
Saul <s.shanabrook@gmail.com>
2014-03-24 22:05:02 -0400

    make eastwood ignore testing paths, breaks on midje

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [prismatic/schema "0.2.1"]
                  [com.taoensso/timbre "3.1.6"]]
   :min-lein-version "2.0.0"
+  :eastwood {:exclude-namespaces [:test-paths]}
   :profiles {:test {:plugins [[jonase/eastwood "0.1.0"]
                               [lein-cloverage "1.0.2"]]
                     :dependencies [[midje "1.5.0"]]}})
